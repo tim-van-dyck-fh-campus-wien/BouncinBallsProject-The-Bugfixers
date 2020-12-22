6c51ae75091684033a9a4805decd9f185992e8c1
timvandycke <tim.van.dyck13@gmail.com>
2020-12-22 20:37:38 +0100

    Initial commit

---
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/Main.java
@@ -0,0 +1,26 @@
+package at.ac.fhcampuswien.bouncingballs;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+
+        Scene scene = new Scene(root, 1280, 720);
+        //primaryStage.setFullScreen(true);
+        // We can add EventHandler also to the scene if appropriate: clicking, exiting...
+
+        // Animation: either Timeline with Keyframe or
+        // AnimationTimer http://zetcode.com/gui/javafx/animation/
+
+        primaryStage.setTitle("Hello Robot");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
