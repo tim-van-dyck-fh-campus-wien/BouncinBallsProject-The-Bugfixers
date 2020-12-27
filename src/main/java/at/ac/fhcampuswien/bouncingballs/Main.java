789035b22c039896c49a074700aa5289aae35cd3
Zeljko Sekulic <zeljko.sekulic@stud.fh-campuswien.ac.at>
2020-12-27 19:43:33 +0100

    Push

--- src/main/java/at/ac/fhcampuswien/bouncingballs/Main.java
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
--- src/main/java/at/ac/fhcampuswien/bouncingballs/controllers/MainController.java
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/controllers/MainController.java
@@ -2,9 +2,12 @@ package at.ac.fhcampuswien.bouncingballs.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -24,18 +27,23 @@ public class MainController implements Initializable  {
     Canvas graph;
     GraphicsContext graphGC;
 
+    @FXML
+    GridPane containerleft;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        this.simulation = new Canvas(500,500);
+        this.simulation = new Canvas(550,550);
         this.simulationGC=this.simulation.getGraphicsContext2D();
-        this.graph = new Canvas(500,500);
+        this.graph = new Canvas(500,400);
+        this.graphContainer.getChildren().add(this.graph);
+        GridPane.setValignment(graphContainer, VPos.BOTTOM);
         this.graphGC=this.graph.getGraphicsContext2D();
         this.simulationGC.setFill(Color.BLACK);
         this.graphGC.setFill(Color.RED);
         this.borderPane.setRight(this.simulation);
 
-        this.graphContainer.getChildren().add(this.graph);
+
 
         this.simulationGC.fillRect(0,0,10000,10000);
         this.graphGC.fillRect(0,0,10000,10000);
