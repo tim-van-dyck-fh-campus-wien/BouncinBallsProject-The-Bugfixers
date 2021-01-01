82042ad9a84fad26bdc03c290affcdb0535b1d81
Zeljko Sekulic <zeljko.sekulic@stud.fh-campuswien.ac.at>
2021-01-01 21:41:38 +0100

    Start Button

--- src/main/java/at/ac/fhcampuswien/bouncingballs/Main.java
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/Main.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/homepage.fxml"));
 
         Scene scene = new Scene(root, 1280, 720);
         scene.getStylesheets().add("/style.css");
--- src/main/java/at/ac/fhcampuswien/bouncingballs/controllers/homepage.java
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/controllers/homepage.java
@@ -1,13 +1,42 @@
 package at.ac.fhcampuswien.bouncingballs.controllers;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class homepage {
 
-    public void manageButton(ActionEvent actionEvent) {
+    @FXML private Text actiontarget;
+    private ResourceBundle resources;
 
+    @FXML protected void manageButton(ActionEvent event) {
 
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"), resources);
+            Stage stage = new Stage();
+            stage.setTitle("Bouncing-Balls");
+            stage.setScene(new Scene(root, 1280, 720));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
+
